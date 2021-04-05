287aa8e08b9da5bb1066dbe75cdde711fec0f7cb
rtharungowda <rtharun.gowda.cd.ece19@itbhu.ac.in>
2021-04-05 10:19:07 +0000

    Add create csv files

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+
+NUM_CLASSES = 36
+TRAIN_IMAGES = "/content/drive/MyDrive/competitions/mosaic-r1/dataset/Train"
+VAL_IMAGES = "/content/drive/MyDrive/competitions/mosaic-r1/dataset/Test"
+TRAIN_CSV = '/content/drive/MyDrive/competitions/mosaic-r1/train.csv'
+VAL_CSV = '/content/drive/MyDrive/competitions/mosaic-r1/val.csv'
+MAPPING = {
+    'character_10_yna':10,
+    'character_11_taamatar':11,
+    'character_12_thaa':12, 
+    'character_13_daa':13, 
+    'character_14_dhaa':14, 
+    'character_15_adna':15, 
+    'character_16_tabala':16, 
+    'character_17_tha':17,
+    'character_18_da':18, 
+    'character_19_dha':19, 
+    'character_1_ka':1, 
+    'character_20_na':20, 
+    'character_21_pa':21, 
+    'character_22_pha':22, 
+    'character_23_ba':23, 
+    'character_24_bha':24, 
+    'character_25_ma':25, 
+    'character_26_yaw':26, 
+    'character_27_ra':27, 
+    'character_28_la':28, 
+    'character_29_waw':29, 
+    'character_2_kha':2, 
+    'character_30_motosaw':30, 
+    'character_31_petchiryakha':31, 
+    'character_32_patalosaw':32, 
+    'character_33_ha':33, 
+    'character_34_chhya':34, 
+    'character_35_tra':35, 
+    'character_36_gya':36, 
+    'character_3_ga':3, 
+    'character_4_gha':4, 
+    'character_5_kna':5, 
+    'character_6_cha':6, 
+    'character_7_chha':7, 
+    'character_8_ja':8, 
+    'character_9_jha':9,
+}
+
+if __name__ == "__main__":
+    print(NUM_CLASSES)
---
+++ prep_csv.py
@@ -0,0 +1,30 @@
+import config
+import glob
+import os
+import pandas as pd
+
+def create_csv(path_dataset,train=True):
+    folders = os.listdir(path_dataset)
+    path = []
+    labels = []
+    for folder in folders:
+        folder_path = os.path.join(path_dataset,folder)
+        files = glob.glob(folder_path+"/*.png")
+        for f in files:
+            # print(f,folder)
+            path.append(f)
+            labels.append(config.MAPPING[folder])
+    data = {
+        'path':path,
+        'label':labels
+    }
+
+    df = pd.DataFrame(data)
+    if train:
+        df.to_csv(config.TRAIN_CSV)
+    else :
+        df.to_csv(config.VAL_CSV)
+
+if __name__ == "__main__":
+    create_csv(config.TRAIN_IMAGES)
+    create_csv(config.VAL_IMAGES,train=False)
