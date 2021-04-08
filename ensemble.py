ff30b967f5a7b9cfc9775c36f06c024326595a86
rtharungowda <rtharun.gowda.cd.ece19@itbhu.ac.in>
2021-04-08 13:22:11 +0000

    start ensemble

--- ensemble.py
+++ ensemble.py
@@ -7,9 +7,8 @@ import numpy as np
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
-from custom_model import akbhd, vatch, drklrd
+from model import akbhd, vatch, drklrd, mdl
 import config
-import custom_model
 
 def check_acc():
     model_ft = akbhd()
@@ -35,16 +34,36 @@ def check_acc():
         _, _, epoch, val_acc = load_ckp(p, model_ft, optimizer_ft, config.DEVICE)
         print(f"{val_acc} {p}")
     
-    model_ft = vatch()
+    model_ft = mdl("res18")
     optimizer_ft = optim.Adam(model_ft.parameters(), lr=0.001)
-    checkpoint_path_vatch = [
-                    "/content/drive/MyDrive/competitions/mosaic-r1/weights/vatch_albu.pt",
-                    "/content/drive/MyDrive/competitions/mosaic-r1/weights/vatch_albu2.pt",
-                    "/content/drive/MyDrive/competitions/mosaic-r1/weights/vatch.pt"
+    checkpoint_path_res18 = [
+                    "/content/drive/MyDrive/competitions/mosaic-r1/weights/res18_albu_2.pt",
+                    "/content/drive/MyDrive/competitions/mosaic-r1/weights/res18_albu.pt",
     ]
-    for p in checkpoint_path_vatch:
+    for p in checkpoint_path_res18:
         _, _, epoch, val_acc = load_ckp(p, model_ft, optimizer_ft, config.DEVICE)
         print(f"{val_acc} {p}")
+
+    model_ft = mdl("res34")
+    optimizer_ft = optim.Adam(model_ft.parameters(), lr=0.001)
+    checkpoint_path_res34 = [
+                    "/content/drive/MyDrive/competitions/mosaic-r1/weights/res34_albu_2.pt",
+                    # "/content/drive/MyDrive/competitions/mosaic-r1/weights/res34_albu.pt",
+    ]
+    for p in checkpoint_path_res34:
+        _, _, epoch, val_acc = load_ckp(p, model_ft, optimizer_ft, config.DEVICE)
+        print(f"{val_acc} {p}")
+
+    # model_ft = vatch()
+    # optimizer_ft = optim.Adam(model_ft.parameters(), lr=0.001)
+    # checkpoint_path_vatch = [
+    #                 "/content/drive/MyDrive/competitions/mosaic-r1/weights/vatch_albu.pt",
+    #                 "/content/drive/MyDrive/competitions/mosaic-r1/weights/vatch_albu2.pt",
+    #                 "/content/drive/MyDrive/competitions/mosaic-r1/weights/vatch.pt"
+    # ]
+    # for p in checkpoint_path_vatch:
+    #     _, _, epoch, val_acc = load_ckp(p, model_ft, optimizer_ft, config.DEVICE)
+    #     print(f"{val_acc} {p}")
 
 if __name__ == "__main__":
     check_acc()
