bad5235e845523da98735edcc977ed30176b96e0
rtharungowda <rtharun.gowda.cd.ece19@itbhu.ac.in>
2021-04-08 15:33:44 +0000

    remove git.sh

--- git.sh
+++
@@ -1,9 +0,0 @@
-for SOME_VAR in $@
-do
-    git config user.name rtharungowda
-    git config user.email rtharun.gowda.cd.ece19@itbhu.ac.in
-
-    git add .
-    git commit -m /"$SOME_VAR/"
-    git push
-done;
